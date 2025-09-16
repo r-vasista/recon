4314c2a1ec3bfd4a2c214ffef0c5c74269e1fb94
vasista <vasista.rachaputi@gmail.com>
2025-09-16 18:12:20 +0530

    created new recon project

---
+++ app/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Portal, PortalCategory, MasterCategory, MasterCategoryMapping
+
+@admin.register(Portal)
+class PortalAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'base_url']
+    search_fields = ['id', 'name', 'base_url']
+    list_filter = ['id', 'name', 'base_url']
+    
+
+@admin.register(PortalCategory)
+class PortalCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'portal', 'name', 'external_id']
+    search_fields =['id', 'name', 'portal', 'external_id']
+    list_filter = ['id', 'name', 'portal', 'external_id']
+
+
+@admin.register(MasterCategory)
+class MasterCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    search_fields = ['id', 'name', 'description']
+    list_filter = ['id', 'name', 'description']
+
+
+@admin.register(MasterCategoryMapping)
+class MasterCategoryMappingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'master_category', 'portal_category']
+    search_fields = ['id', 'master_category', 'portal_category']
+    list_filter = ['id', 'master_category', 'portal_category']
---
+++ app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    PortalListCreateView, PortalDetailView, PortalCategoryCreateView, PortalCategoryUpdateDeleteView,
+    PortalCategoryListView, MasterCategoryView, MasterCategoryMappingView, MasterCategoryMappingsListView
+)
+
+urlpatterns = [
+    # Portals
+    path('portals/list/', PortalListCreateView.as_view()),
+    path('portal/detail/<int:id>/', PortalDetailView.as_view()),
+    
+    # Portal Category
+    path('portal/category/', PortalCategoryCreateView.as_view()),
+    path('portal/category/<str:portal_name>/<str:external_id>/', PortalCategoryUpdateDeleteView.as_view()),
+    path('portals/categories/list/<str:portal_name>/', PortalCategoryListView.as_view()),
+    
+    # Master Category
+    path('master/category/', MasterCategoryView.as_view()),
+    path('master/category/<int:pk>/', MasterCategoryView.as_view()),
+    path('master/category/mapping/', MasterCategoryMappingView.as_view()),
+    path('master/category/mapping/<int:pk>/', MasterCategoryMappingView.as_view()),
+    path('master/categories/mapped/<int:master_category_id>/', MasterCategoryMappingsListView.as_view()),
+]
