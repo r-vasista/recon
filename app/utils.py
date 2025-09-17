9528fac8709d5a0169155107148dc4bee4b82043
vasista <vasista.rachaputi@gmail.com>
2025-09-17 12:32:54 +0530

    ignoring all unwanted files

--- app/utils.py
+++
@@ -1,5 +0,0 @@
-def success_response(data, message = None):
-    return {"status": True, "data":data, "message":message}
-
-def error_response(message):
-    return {"status": False, "message":message}
