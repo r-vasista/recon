fd0c2cbe6da2014a526fc874886d7a527392dfa4
vasista <vasista.rachaputi@gmail.com>
2025-09-17 12:33:09 +0530

    adding all needed files

---
+++ app/utils.py
@@ -0,0 +1,5 @@
+def success_response(data, message = None):
+    return {"status": True, "data":data, "message":message}
+
+def error_response(message):
+    return {"status": False, "message":message}
