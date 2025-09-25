38e90351071b3dfcd92db0ac4aeb073b5472601b
vasista <vasista.rachaputi@gmail.com>
2025-09-25 16:35:14 +0530

    added cors in settings

--- recon/settings.py
+++ recon/settings.py
@@ -47,9 +47,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'app',
     'user',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'recon.urls'
 
