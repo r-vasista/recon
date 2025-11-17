a50e2f84edc33fa2dd34ecf9db11641a77a5415d
vasista <vasista.rachaputi@gmail.com>
2025-11-17 17:26:08 +0530

    created model for user portal assignment

--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import (
-    PortalUserMapping, UserCategoryGroupAssignment, UserRole, Role
+    PortalUserMapping, UserCategoryGroupAssignment, UserRole, Role, UserPortalAssignment
 )
 
 @admin.register(PortalUserMapping)
@@ -41,3 +41,9 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ("is_staff", "is_superuser", "is_active")
     search_fields = ("username", "email", "first_name", "last_name")
     ordering = ("id",)
+    
+@admin.register(UserPortalAssignment)
+class UserPortalAssignmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'portal']
+    search_fields = ['id', 'user', 'portal']
+    list_filter =  ['id', 'user', 'portal']
