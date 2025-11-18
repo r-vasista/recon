f42ca9b9ef2003466831c3d1ed2aa003d70c263f
techswapnil1998 <swapnilshrivastava794@gmail.com>
2025-11-18 15:29:25 +0530

    aa

---
+++ ga4/request.py
@@ -0,0 +1,25 @@
+# ga4/request.py
+
+import requests
+from ga4.utils import generate_ga4_token
+
+def ga4_request(property_id, endpoint, body):
+    """
+    Fully Dynamic GA4 API Caller
+    endpoint: runReport, runRealtimeReport, runPivotReport, runFunnelReport...
+    body: ANY GA4 request body
+    """
+    token = generate_ga4_token()
+    if not token:
+        return {"error": "Token generation failed"}
+
+    url = f"https://analyticsdata.googleapis.com/v1beta/properties/{property_id}:{endpoint}"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+
+    return response.json()
